ee4ea2a89b70311c08d92ab9e37f9aa581141546
RehanAhmad3553 <117620259+RehanAhmad3553@users.noreply.github.com>
2023-03-12 19:06:47 +0500

    Notes

---
+++ app/src/main/java/com/example/recyclerinkotlin/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.recyclerinkotlin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        //write something here
+        Toast.makeText(this, "HelloWorld", Toast.LENGTH_SHORT).show();
+    }
+}
